681a3e68cba9e16c09ca7240abb0fcf56522efc5
fkanegae <fkanegae@gmail.com>
2025-03-15 23:05:32 +0100

    landing page adjustments

---
+++ src/lib/config.ts
@@ -0,0 +1,36 @@
+export const siteConfig = {
+  faqs: [
+    {
+      question: "What kind of photos do I need to upload?",
+      answer: "Make variety a priority. Varied facial expressions and varied backgrounds, taken at various times of the day, are the keys to high quality input photos. Oh, and minimal makeup and accessories, please!"
+    },
+    {
+      question: "What do you do with my uploaded photos?",
+      answer: "The photos you upload are used to train our AI model so it can create realistic AI headshots. These input photos are deleted within 7 days, but you can instantly delete them at any time with our 'Delete' button."
+    },
+    {
+      question: "Who owns my AI photos?",
+      answer: "You do. We grant you full commercial license and ownership over your photos."
+    },
+    {
+      question: "What if I don't like my photos?",
+      answer: "No problem. We promise 3-6 keepers in every order. If you don't find at least 3 incredible headshots, just don't download any of your results and we'll refund you in full."
+    },
+    {
+      question: "How long does an AI headshot take?",
+      answer: "We don't cut corners when it comes to generating photorealistic AI headshots. Our Basic package delivers 10 photos in about 10 minutes, while Professional and Executive packages can take a bit longer since they generate up to 100-200 photos. But no matter which package you choose, you'll always get your results in just a few minutes."
+    },
+    {
+      question: "What do people misunderstand about AI headshots?",
+      answer: "Not every photo is perfect. Due to the nature of AI, you might see some strange photos. YouPhotoshoot tries to make this clear from the start: not every photo is perfect, but we promise you'll find a profile-worthy headshot in every order to make it all worth it."
+    },
+    {
+      question: "How many good photos can I expect?",
+      answer: "The amount of keeper headshots you get back will largely depend on the photos you provide us with. Customers who make an effort to follow the instructions closely often walk away with 8-10+ incredible photos. At the very least, we guarantee you'll get a Profile-Worthy headshot back."
+    },
+    {
+      question: "Can I use AI headshots on LinkedIn?",
+      answer: "25% of YouPhotoshoot customers use their AI headshots on LinkedIn. It's totally okay to use AI headshots on LinkedIn."
+    },
+  ]
+}; 
